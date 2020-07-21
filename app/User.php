765b236e8a88da6a7efdb50a40d89e62c1d3aa41
unknown <mchrezky@gmail.com>
2020-07-21 21:21:36 +0700

    ralat typo field model dev_address

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'id', 'name', 'email', 'email_verified_at', 'password', 'picture', 'role_id', 'token', 'dev_web', 'dev_country_id', 'dev_adrress', 'eu_birthday', 'eu_device_id', 'is_verified', 'is_blocked', 'created_by', 'updated_by', 'remember_token', 'created_at', 'updated_at'
+        'id', 'name', 'email', 'email_verified_at', 'password', 'picture', 'role_id', 'token', 'dev_web', 'dev_country_id', 'dev_address', 'eu_birthday', 'eu_device_id', 'is_verified', 'is_blocked', 'created_by', 'updated_by', 'remember_token', 'created_at', 'updated_at'
     ];
 
     /**
