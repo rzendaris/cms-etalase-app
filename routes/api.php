9afaaf91041e67fcebe2f257219f75083af0fec0
Rafi Zendaris <rafi@ajaib.co.id>
2020-08-16 01:02:09 +0700

    Remove validation env on api routes

--- routes/api.php
+++ routes/api.php
@@ -16,31 +16,29 @@ use Illuminate\Http\Request;
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
-if (env('ENV') == 'API'){
-    Route::post('v1/login', 'API\v1\APIAuthController@login');
-    Route::post('v1/register', 'API\v1\APIAuthController@register');
-    Route::post('v1/forgot-password', 'API\v1\APIAuthController@forgotPassword');
+Route::post('v1/login', 'API\v1\APIAuthController@login');
+Route::post('v1/register', 'API\v1\APIAuthController@register');
+Route::post('v1/forgot-password', 'API\v1\APIAuthController@forgotPassword');
 
-    Route::group(['middleware' => 'auth.api'], function() {
-        /**
-         * API Version 1
-         */
-        Route::group(['prefix' => 'v1'], function () {
-            Route::get('logout', 'API\v1\APIAuthController@logout');
-            Route::get('user-info', 'API\v1\APIAuthController@user');
-            
-            Route::get('apps', 'API\v1\AppsController@GetAllApps');
-            Route::get('apps/{id}/detail', 'API\v1\AppsController@AppDetail');
-            Route::get('apps/action/{action}/{apps_id}', 'API\v1\AppsController@AppsAction');
-            Route::get('apps/list-category', 'API\v1\AppsController@GetAppsCategory');
+Route::group(['middleware' => 'auth.api'], function() {
+    /**
+     * API Version 1
+     */
+    Route::group(['prefix' => 'v1'], function () {
+        Route::get('logout', 'API\v1\APIAuthController@logout');
+        Route::get('user-info', 'API\v1\APIAuthController@user');
+        
+        Route::get('apps', 'API\v1\AppsController@GetAllApps');
+        Route::get('apps/{id}/detail', 'API\v1\AppsController@AppDetail');
+        Route::get('apps/action/{action}/{apps_id}', 'API\v1\AppsController@AppsAction');
+        Route::get('apps/list-category', 'API\v1\AppsController@GetAppsCategory');
 
-            Route::get('apps/installed', 'API\v1\AppsController@user');
-            Route::get('apps/download', 'API\v1\AppsController@user');
-            Route::get('apps/update', 'API\v1\AppsController@user');
-            Route::get('apps/category/{id}', 'API\v1\AppsController@user');
-        });
+        Route::get('apps/installed', 'API\v1\AppsController@user');
+        Route::get('apps/download', 'API\v1\AppsController@user');
+        Route::get('apps/update', 'API\v1\AppsController@user');
+        Route::get('apps/category/{id}', 'API\v1\AppsController@user');
     });
-}
+});
 Route::any('{path}', function() {
     return response()->json([
         'message' => 'Route not found'
