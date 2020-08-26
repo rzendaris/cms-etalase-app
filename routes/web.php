c6bfff0b8046e2d606addb981be8e44ee92b159a
fahmi123 <fahmiahmadi123@gmail.com>
2020-08-26 23:52:20 +0700

    Create Developer Pages *app management *feedbacks and reply

--- routes/web.php
+++ routes/web.php
@@ -182,5 +182,8 @@ Route::post('change-password', 'Auth\ForgotPasswordController@changePassword');
 // Route::post('user-management-cms/reset', 'Cms\UserManController@UserMgmtResetPass');
 // Route::get('user-management-cms/profile', 'Cms\UserManController@UserMgmtProfile');
 Route::get('register-dev', 'Auth\RegisterController@getRegister');
-// FE Route Dummy
-// Route::get('register-page', 'FeController@Register');
+// FE Route Dummy Route::get('register-page', 'FeController@Register');
+Route::get('feedbacks-and-reply', 'FeController@Feedbacks');
+Route::get('add-application-developer', 'FeController@DevAdd');
+Route::get('edit-application-developer', 'FeController@DevEdit');
+Route::get('detail-application-developer', 'FeController@DevDetail');
