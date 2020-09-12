1cdc6b0f5fe88b20d3452806953a398729d30a8b
unknown <mchrezky@gmail.com>
2020-09-12 19:41:36 +0700

    fix bug regiterdeveloper image dir, fix back button app-dev, update type apps

--- resources/views/apps-developer/detail.blade.php
+++ resources/views/apps-developer/detail.blade.php
@@ -171,7 +171,7 @@
     <hr>
     <div class="row">
         <div class="col-xl-12 col-md-12 m-b-10px text-right">
-            <a href="{{ url('apps-management') }}" class="btn btn-danger pull-left">Back</a>
+            <a href="{{ url('apps-developer') }}" class="btn btn-danger pull-left">Back</a>
         </div>
     </div>
 
